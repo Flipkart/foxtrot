bc04d48652f45c3bca942eb28d8016e61b596a31
Santanu Sinha <santanu.sinha@flipkart.com>
2014-03-27 20:37:50 +0530

    Added subtype for group

--- foxtrot-common/src/main/java/com/flipkart/foxtrot/common/ActionRequest.java
+++ foxtrot-common/src/main/java/com/flipkart/foxtrot/common/ActionRequest.java
@@ -2,6 +2,7 @@ package com.flipkart.foxtrot.common;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.flipkart.foxtrot.common.group.GroupRequest;
 import com.flipkart.foxtrot.common.query.Query;
 
 /**
@@ -11,7 +12,8 @@ import com.flipkart.foxtrot.common.query.Query;
  */
 @JsonTypeInfo(use= JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "opcode")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = Query.class, name= "query")
+        @JsonSubTypes.Type(value = Query.class, name= "query"),
+        @JsonSubTypes.Type(value = GroupRequest.class, name= "group")
 })
 public interface ActionRequest {
 }
