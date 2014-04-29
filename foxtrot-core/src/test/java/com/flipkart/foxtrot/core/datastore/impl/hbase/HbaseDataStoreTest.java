252023bef2dcd3abef4ff8b64e56767770c70056
Rishabh Goyal <rishabh.goyal@flipkart.com>
2014-04-29 12:46:20 +0530

    Changes +Tests for Various DataStoreExceptions

--- foxtrot-core/src/test/java/com/flipkart/foxtrot/core/datastore/impl/hbase/HbaseDataStoreTest.java
+++ foxtrot-core/src/test/java/com/flipkart/foxtrot/core/datastore/impl/hbase/HbaseDataStoreTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flipkart.foxtrot.common.Document;
 import com.flipkart.foxtrot.core.MockHTable;
+import com.flipkart.foxtrot.core.datastore.DataStoreException;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
@@ -12,10 +13,12 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.*;
 
 import static org.junit.Assert.*;
@@ -28,7 +31,6 @@ import static org.mockito.Mockito.*;
 public class HbaseDataStoreTest {
     private HbaseDataStore hbaseDataStore;
     private HTableInterface tableInterface;
-    private HbaseTableConnection tableConnection;
     private final ObjectMapper mapper = new ObjectMapper();
 
     private static final Logger logger = LoggerFactory.getLogger(HbaseDataStoreTest.class.getSimpleName());
@@ -39,7 +41,8 @@ public class HbaseDataStoreTest {
     @Before
     public void setUp() throws Exception {
         this.tableInterface = MockHTable.create();
-        this.tableConnection = Mockito.mock(HbaseTableConnection.class);
+        this.tableInterface = spy(this.tableInterface);
+        HbaseTableConnection tableConnection = Mockito.mock(HbaseTableConnection.class);
         when(tableConnection.getTable()).thenReturn(tableInterface);
         hbaseDataStore = new HbaseDataStore(tableConnection, mapper);
     }
@@ -58,7 +61,22 @@ public class HbaseDataStoreTest {
         JsonNode data = mapper.valueToTree(Collections.singletonMap("TEST_NAME", "SINGLE_SAVE_TEST"));
         expectedDocument.setData(data);
         hbaseDataStore.save(TEST_APP, expectedDocument);
-        verify(tableConnection, times(1)).getTable();
+        validateSave(expectedDocument);
+        logger.info("Tested Single Save");
+    }
+
+    @Test(expected = DataStoreException.class)
+    public void testSaveSingleException() throws Throwable {
+        logger.info("Testing Single Save - Exception");
+        Document expectedDocument = new Document();
+        expectedDocument.setId(UUID.randomUUID().toString());
+        expectedDocument.setTimestamp(System.currentTimeMillis());
+        JsonNode data = mapper.valueToTree(Collections.singletonMap("TEST_NAME", "SINGLE_SAVE_TEST"));
+        expectedDocument.setData(data);
+        doThrow(new IOException())
+                .when(tableInterface)
+                .put(Matchers.<Put>any());
+        hbaseDataStore.save(TEST_APP, expectedDocument);
         validateSave(expectedDocument);
         logger.info("Tested Single Save");
     }
@@ -73,7 +91,25 @@ public class HbaseDataStoreTest {
                     mapper.valueToTree(Collections.singletonMap("TEST_NAME", "SINGLE_SAVE_TEST"))));
         }
         hbaseDataStore.save(TEST_APP, documents);
-        verify(tableConnection, times(1)).getTable();
+        for (Document document : documents) {
+            validateSave(document);
+        }
+        logger.info("Tested Bulk Save");
+    }
+
+    @Test(expected = DataStoreException.class)
+    public void testSaveBulkException() throws Exception {
+        logger.info("Testing Bulk Save - Exception");
+        List<Document> documents = new Vector<Document>();
+        for (int i = 0; i < 10; i++) {
+            documents.add(new Document(UUID.randomUUID().toString(),
+                    System.currentTimeMillis(),
+                    mapper.valueToTree(Collections.singletonMap("TEST_NAME", "SINGLE_SAVE_TEST"))));
+        }
+        doThrow(new IOException())
+                .when(tableInterface)
+                .put(Matchers.<Put>any());
+        hbaseDataStore.save(TEST_APP, documents);
         for (Document document : documents) {
             validateSave(document);
         }
@@ -100,9 +136,31 @@ public class HbaseDataStoreTest {
         Document expectedDocument = new Document(id, System.currentTimeMillis(), data);
         tableInterface.put(hbaseDataStore.getPutForDocument(TEST_APP, expectedDocument));
         Document actualDocument = hbaseDataStore.get(TEST_APP, id);
-        verify(tableConnection, times(1)).getTable();
         compare(expectedDocument, actualDocument);
         logger.info("Tested Single Get");
+    }
+
+    @Test(expected = DataStoreException.class)
+    public void testGetSingleMissingDocument() throws Throwable {
+        logger.info("Testing Single Get - Missing ID");
+        hbaseDataStore.get(TEST_APP, UUID.randomUUID().toString());
+        logger.info("Tested Single Get - Missing ID");
+    }
+
+    @Test(expected = DataStoreException.class)
+    public void testGetSingleException() throws Throwable {
+        logger.info("Testing Single Get - Exception");
+        String id = UUID.randomUUID().toString();
+        JsonNode data = mapper.valueToTree(Collections.singletonMap("TEST_NAME", "SINGLE_SAVE_TEST"));
+
+        Document expectedDocument = new Document(id, System.currentTimeMillis(), data);
+        tableInterface.put(hbaseDataStore.getPutForDocument(TEST_APP, expectedDocument));
+        doThrow(new IOException())
+                .when(tableInterface)
+                .get(Matchers.<Get>any());
+        Document actualDocument = hbaseDataStore.get(TEST_APP, id);
+        compare(expectedDocument, actualDocument);
+        logger.info("Tested Single Get - Exception");
     }
 
     @Test
@@ -121,7 +179,6 @@ public class HbaseDataStoreTest {
         tableInterface.put(putList);
 
         List<Document> actualDocuments = hbaseDataStore.get(TEST_APP, ids);
-        verify(tableConnection, times(1)).getTable();
         HashMap<String, Document> actualIdValues = new HashMap<String, Document>();
         for (Document doc : actualDocuments) {
             actualIdValues.put(doc.getId(), doc);
@@ -132,6 +189,43 @@ public class HbaseDataStoreTest {
             compare(idValues.get(id), actualIdValues.get(id));
         }
         logger.info("Tested Bulk Get");
+    }
+
+    @Test(expected = DataStoreException.class)
+    public void testGetBulkMissingDocument() throws Throwable {
+        logger.info("Testing Bulk Get - Missing ID");
+        hbaseDataStore.get(TEST_APP, Arrays.asList(UUID.randomUUID().toString(), UUID.randomUUID().toString()));
+        logger.info("Tested Bulk Get - Missing ID");
+    }
+
+    @Test(expected = DataStoreException.class)
+    public void testGetBulkException() throws Throwable {
+        logger.info("Testing Bulk Get - Exception");
+        Map<String, Document> idValues = new HashMap<String, Document>();
+        List<String> ids = new Vector<String>();
+        List<Put> putList = new Vector<Put>();
+        for (int i = 0; i < 10; i++) {
+            String id = UUID.randomUUID().toString();
+            ids.add(id);
+            idValues.put(id,
+                    new Document(id, System.currentTimeMillis(), mapper.valueToTree(Collections.singletonMap("TEST_NAME", "SINGLE_SAVE_TEST"))));
+            putList.add(hbaseDataStore.getPutForDocument(TEST_APP, idValues.get(id)));
+        }
+        tableInterface.put(putList);
+        doThrow(new IOException())
+                .when(tableInterface)
+                .get(Matchers.<Get>any());
+        List<Document> actualDocuments = hbaseDataStore.get(TEST_APP, ids);
+        HashMap<String, Document> actualIdValues = new HashMap<String, Document>();
+        for (Document doc : actualDocuments) {
+            actualIdValues.put(doc.getId(), doc);
+        }
+        assertNotNull("List of returned Documents should not be null", actualDocuments);
+        for (String id : ids) {
+            assertTrue("Requested Id should be present in response", actualIdValues.containsKey(id));
+            compare(idValues.get(id), actualIdValues.get(id));
+        }
+        logger.info("Tested Bulk Get - Exception");
     }
 
 
