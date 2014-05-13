b633c641c1c322fdc65caaee983fbacf3c0e502a
Rishabh Goyal <rishabh.goyal@flipkart.com>
2014-05-13 14:18:00 +0530

    Added logging to TableFieldMappingResource

--- foxtrot-server/src/main/java/com/flipkart/foxtrot/server/resources/TableFieldMappingResource.java
+++ foxtrot-server/src/main/java/com/flipkart/foxtrot/server/resources/TableFieldMappingResource.java
@@ -2,6 +2,8 @@ package com.flipkart.foxtrot.server.resources;
 
 import com.flipkart.foxtrot.core.querystore.QueryStore;
 import com.flipkart.foxtrot.core.querystore.QueryStoreException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -14,7 +16,7 @@ import java.util.Collections;
 @Path("/foxtrot/v1/tables/{name}/fields")
 @Produces(MediaType.APPLICATION_JSON)
 public class TableFieldMappingResource {
-
+    private static final Logger logger = LoggerFactory.getLogger(TableFieldMappingResource.class.getSimpleName());
     private QueryStore queryStore;
 
     public TableFieldMappingResource(QueryStore queryStore) {
@@ -26,6 +28,7 @@ public class TableFieldMappingResource {
         try {
             return Response.ok(queryStore.getFieldMappings(table)).build();
         } catch (QueryStoreException ex) {
+            logger.error("Unable to fetch Table Metadata " , ex);
             throw new WebApplicationException(Response.serverError()
                     .entity(Collections.singletonMap("error", "Metadata Fetch Failed")).build());
         }
