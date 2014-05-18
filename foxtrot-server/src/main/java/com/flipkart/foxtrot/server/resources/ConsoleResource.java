85cb55e04619462635d7f1e44245901425d2b398
Santanu Sinha <santanu.sinha@flipkart.com>
2014-05-18 22:12:36 +0530

    Changes console resource url path

--- foxtrot-server/src/main/java/com/flipkart/foxtrot/server/resources/ConsoleResource.java
+++ foxtrot-server/src/main/java/com/flipkart/foxtrot/server/resources/ConsoleResource.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Response;
 import java.util.Collections;
 import java.util.List;
 
-@Path("/foxtrot/v1/analytics")
+@Path("/foxtrot/v1/consoles")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ConsoleResource {
