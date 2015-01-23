6bf4917964b6126b6d0eb19042276856d3829cef
Rishabh Goyal <rishabh.goyal@flipkart.com>
2015-01-23 10:24:00 +0530

    Added tile for flattened fql query data

--- foxtrot-server/src/main/resources/console/js/consolemanager.js
+++ foxtrot-server/src/main/resources/console/js/consolemanager.js
@@ -18,88 +18,84 @@ function TileFactory() {
 }
 
 TileFactory.create = function(type) {
-	if(type === "donut") {
-		return new DonutTile();
-	}
-	else if(type === "bar") {
-		return new BarTile();
-	}
-	else if(type === "line") {
-		return new LineTile();
-	}
-	else if(type === "histogram") {
-		return new Histogram();
-	}
-	else if(type === "eventbrowser") {
-		return new EventBrowser();
-	}
-	else if(type === "stacked_bar") {
-		return new StackedBar();
-	}
-	else if(type === "statstrend") {
-		return new StatsTrend();
-	}
+    if (type === "donut") {
+        return new DonutTile();
+    } else if (type === "bar") {
+        return new BarTile();
+    } else if (type === "line") {
+        return new LineTile();
+    } else if (type === "histogram") {
+        return new Histogram();
+    } else if (type === "eventbrowser") {
+        return new EventBrowser();
+    } else if (type === "stacked_bar") {
+        return new StackedBar();
+    } else if (type === "statstrend") {
+        return new StatsTrend();
+    } else if (type === 'fql_table') {
+        return new FqlTable()
+    }
+};
+
+function ConsoleManager(tileSet, queue, tables) {
+    this.tileSet = tileSet;
+    this.queue = queue;
+    this.tables = tables;
 }
 
-function ConsoleManager (tileSet, queue, tables) {
-	this.tileSet = tileSet;
-	this.queue = queue;
-	this.tables = tables;
-}
+ConsoleManager.prototype.getConsoleRepresentation = function () {
+    var tiles = {};
+    for (var tile in this.tileSet.currentTiles) {
+        if (this.tileSet.currentTiles.hasOwnProperty(tile)) {
+            var representation = this.tileSet.currentTiles[tile].getRepresentation();
+            tiles[representation.id] = representation;
+        }
+    }
+    var tileList = [];
+    var tileDivs = $("#tileContainer").find(".tile");
+    for (var i = 0; i < tileDivs.length; i++) {
+        tileList.push(tileDivs[i].id);
+    }
+    representation = {
+        tileList: tileList,
+        tiles: tiles
+    };
+    if ($('#pinConsoleToApp').is(":checked")) {
+        if (this.tables.selectedTable) {
+            representation['appName'] = this.tables.selectedTable.name;
+        }
+    }
+    return representation;
+};
 
-ConsoleManager.prototype.getConsoleRepresentation = function() {
-	var tiles = new Object();
-	for(tile in this.tileSet.currentTiles) {
-		if(this.tileSet.currentTiles.hasOwnProperty(tile)) {
-			var representation = this.tileSet.currentTiles[tile].getRepresentation();
-			tiles[representation.id] = representation;
-		}
-	}
-	var tileList = [];
-	var tileDivs = $("#tileContainer").find(".tile");
-	for (var i = 0; i < tileDivs.length; i++) {
-		tileList.push(tileDivs[i].id);
-	}
-	var representation = {
-		tileList: tileList,
-		tiles: tiles
-	}
-	if($('#pinConsoleToApp').is(":checked")) {
-	    if(this.tables.selectedTable) {
-    	    representation['appName'] = this.tables.selectedTable.name;
-	    }
-	}
-	return representation;
-}
-
-ConsoleManager.prototype.buildConsoleFromRepresentation = function(representation) {
-	var tiles = representation.tiles;
-	var tileList = representation.tileList;
-	for (var i = 0; i < tileList.length; i++) {
-		var tileId = tileList[i];
-		var tileRepresentation = tiles[tileId];
-		var tile = TileFactory.create(tileRepresentation.typeName);
-		tile.loadTileFromRepresentation(tileRepresentation);
-		tile.init(tileRepresentation.id, this.queue, this.tables);
-		this.tileSet.register(tile);
-	}
-	var modal = $("#saveConsoleModal");
-	modal.find(".console-name").val(representation.name);
-	$(".console-name").text(representation.name);
-	if(representation.hasOwnProperty('appName') && representation['appName']) {
+ConsoleManager.prototype.buildConsoleFromRepresentation = function (representation) {
+    var tiles = representation.tiles;
+    var tileList = representation.tileList;
+    for (var i = 0; i < tileList.length; i++) {
+        var tileId = tileList[i];
+        var tileRepresentation = tiles[tileId];
+        var tile = TileFactory.create(tileRepresentation.typeName);
+        tile.loadTileFromRepresentation(tileRepresentation);
+        tile.init(tileRepresentation.id, this.queue, this.tables);
+        this.tileSet.register(tile);
+    }
+    var modal = $("#saveConsoleModal");
+    modal.find(".console-name").val(representation.name);
+    $(".console-name").text(representation.name);
+    if (representation.hasOwnProperty('appName') && representation['appName']) {
         this.tables.forceSelectedTableAfterInit(representation['appName']);
         var tables = this.tables.tables;
-        if(tables && tables.length > 0) {
+        if (tables && tables.length > 0) {
             for (var i = 0; i < tables.length; i++) {
-                if(tables[i].name === representation['appName']) {
+                if (tables[i].name === representation['appName']) {
                     var tableSelect = $("#tables");
                     tableSelect.val(i);
                     tableSelect.change();
                 }
             }
         }
-	}
-	if(this.tables.selectedTable) {
-    	this.queue.executeCalls();
-	}
-}
+    }
+    if (this.tables.selectedTable) {
+        this.queue.executeCalls();
+    }
+};
