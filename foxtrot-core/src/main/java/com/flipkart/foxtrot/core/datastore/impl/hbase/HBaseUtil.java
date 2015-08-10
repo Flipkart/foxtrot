14fb1446c655860fd20723f3713ca8ab0d6111a0
Rishabh Goyal <rishabh.goyal@flipkart.com>
2015-08-10 13:45:20 +0530

    moved basic hbase configuration files out of secure cluster check

--- foxtrot-core/src/main/java/com/flipkart/foxtrot/core/datastore/impl/hbase/HBaseUtil.java
+++ foxtrot-core/src/main/java/com/flipkart/foxtrot/core/datastore/impl/hbase/HBaseUtil.java
@@ -34,27 +34,40 @@ public abstract class HBaseUtil {
 
     public static Configuration create(final HbaseConfig hbaseConfig) throws IOException {
         Configuration configuration = HBaseConfiguration.create();
-        if(hbaseConfig.isSecure()
-                && null != hbaseConfig.getKeytabFileName()
-                && !hbaseConfig.getKeytabFileName().isEmpty()) {
-            File file = new File(hbaseConfig.getKeytabFileName());
-            if (file.exists()) {
-                configuration.addResource(new File(hbaseConfig.getCoreSite()).toURI().toURL());
-                configuration.addResource(new File(hbaseConfig.getHdfsSite()).toURI().toURL());
-                configuration.addResource(new File(hbaseConfig.getHbasePolicy()).toURI().toURL());
-                configuration.addResource(new File(hbaseConfig.getHbaseSite()).toURI().toURL());
-                configuration.set("hbase.master.kerberos.principal", hbaseConfig.getAuthString());
-                configuration.set("hadoop.kerberos.kinit.command", hbaseConfig.getKinitPath());
-                UserGroupInformation.setConfiguration(configuration);
-                System.setProperty("java.security.krb5.conf", hbaseConfig.getKerberosConfigFile());
-                if (hbaseConfig.isSecure()) {
-                    UserGroupInformation.loginUserFromKeytab(
-                            hbaseConfig.getAuthString(), hbaseConfig.getKeytabFileName());
-                    logger.info("Logged into Hbase with User: " + UserGroupInformation.getLoginUser());
-                }
+
+        if (isValidFile(hbaseConfig.getCoreSite())) {
+            configuration.addResource(new File(hbaseConfig.getCoreSite()).toURI().toURL());
+        }
+
+        if (isValidFile(hbaseConfig.getHdfsSite())) {
+            configuration.addResource(new File(hbaseConfig.getHdfsSite()).toURI().toURL());
+        }
+
+        if (isValidFile(hbaseConfig.getHbasePolicy())) {
+            configuration.addResource(new File(hbaseConfig.getHbasePolicy()).toURI().toURL());
+        }
+
+        if (isValidFile(hbaseConfig.getHbaseSite())) {
+            configuration.addResource(new File(hbaseConfig.getHbaseSite()).toURI().toURL());
+        }
+
+        if (hbaseConfig.isSecure() && isValidFile(hbaseConfig.getKeytabFileName())) {
+            configuration.set("hbase.master.kerberos.principal", hbaseConfig.getAuthString());
+            configuration.set("hadoop.kerberos.kinit.command", hbaseConfig.getKinitPath());
+            UserGroupInformation.setConfiguration(configuration);
+            System.setProperty("java.security.krb5.conf", hbaseConfig.getKerberosConfigFile());
+            if (hbaseConfig.isSecure()) {
+                UserGroupInformation.loginUserFromKeytab(
+                        hbaseConfig.getAuthString(), hbaseConfig.getKeytabFileName());
+                logger.info("Logged into Hbase with User: " + UserGroupInformation.getLoginUser());
             }
         }
         return configuration;
+    }
+
+    public static boolean isValidFile(String fileName) {
+        return fileName != null && !fileName.trim().isEmpty() &&
+                new File(fileName).exists();
     }
 
     public static void createTable(final HbaseConfig hbaseConfig, final String tableName) throws IOException {
@@ -66,7 +79,7 @@ public abstract class HBaseUtil {
         try {
             hBaseAdmin.createTable(hTableDescriptor);
         } catch (Exception e) {
-          logger.error("Could not create table: " + tableName, e);
+            logger.error("Could not create table: " + tableName, e);
         } finally {
             hBaseAdmin.close();
         }
