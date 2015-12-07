dc85d866555ea7f5fbd321f28f7506aaafe52c3c
Rishabh Goyal <rishabh.goyal@flipkart.com>
2015-12-07 15:59:49 +0530

    Corrected Table Existence Check

--- foxtrot-core/src/main/java/com/flipkart/foxtrot/core/datastore/impl/hbase/HbaseTableConnection.java
+++ foxtrot-core/src/main/java/com/flipkart/foxtrot/core/datastore/impl/hbase/HbaseTableConnection.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014 Flipkart Internet Pvt. Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,12 +21,16 @@ import com.flipkart.foxtrot.core.util.TableUtil;
 import com.yammer.dropwizard.lifecycle.Managed;
 import net.sourceforge.cobertura.CoverageIgnore;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableNotFoundException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.HTablePool;
 import org.apache.hadoop.hbase.util.PoolMap;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * User: Santanu Sinha (santanu.sinha@flipkart.com)
@@ -39,6 +43,7 @@ public class HbaseTableConnection implements Managed {
 
     private final HbaseConfig hbaseConfig;
     private HTablePool tablePool;
+    private HBaseAdmin hBaseAdmin;
 
     public HbaseTableConnection(HbaseConfig hbaseConfig) {
         this.hbaseConfig = hbaseConfig;
@@ -50,17 +55,28 @@ public class HbaseTableConnection implements Managed {
                 UserGroupInformation.getCurrentUser().reloginFromKeytab();
             }
             return tablePool.getTable(TableUtil.getTableName(hbaseConfig, table));
+        } catch (TableNotFoundException e) {
+            logger.error("TableNotFound" + table.getClass());
+            throw new DataStoreException(DataStoreException.ErrorCode.TABLE_NOT_FOUND,
+                    e.getMessage(), e);
         } catch (Throwable t) {
+            logger.error("SomeOtherException" + t.getClass());
             throw new DataStoreException(DataStoreException.ErrorCode.STORE_CONNECTION,
                     t.getMessage(), t);
         }
+    }
+
+    public synchronized boolean isTableAvailable(final Table table) throws IOException {
+        String tableName = TableUtil.getTableName(hbaseConfig, table);
+        return hBaseAdmin.isTableAvailable(tableName);
     }
 
     @Override
     public void start() throws Exception {
         logger.info("Starting HBase Connection");
         Configuration configuration = HBaseUtil.create(hbaseConfig);
-        tablePool = new HTablePool(configuration, 10, PoolMap.PoolType.Reusable);
+        this.tablePool = new HTablePool(configuration, 10, PoolMap.PoolType.Reusable);
+        this.hBaseAdmin = new HBaseAdmin(configuration);
         logger.info("Started HBase Connection");
     }
 
