4cabb75f909a3ca06fadcda30ec043d876d77c11
Rishabh Goyal <rishabh.goyal@flipkart.com>
2016-01-16 01:25:34 +0530

    Added validation at query executor

--- foxtrot-core/src/main/java/com/flipkart/foxtrot/core/querystore/QueryExecutor.java
+++ foxtrot-core/src/main/java/com/flipkart/foxtrot/core/querystore/QueryExecutor.java
@@ -17,14 +17,15 @@ package com.flipkart.foxtrot.core.querystore;
 
 import com.flipkart.foxtrot.common.ActionRequest;
 import com.flipkart.foxtrot.common.ActionResponse;
+import com.flipkart.foxtrot.common.util.CollectionUtils;
 import com.flipkart.foxtrot.core.common.Action;
 import com.flipkart.foxtrot.core.common.AsyncDataToken;
-import com.flipkart.foxtrot.core.exception.FoxtrotExceptions;
 import com.flipkart.foxtrot.core.exception.FoxtrotException;
+import com.flipkart.foxtrot.core.exception.FoxtrotExceptions;
 import com.flipkart.foxtrot.core.querystore.actions.spi.AnalyticsLoader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 
 /**
@@ -34,7 +35,6 @@ import java.util.concurrent.ExecutorService;
  */
 
 public class QueryExecutor {
-    private static final Logger logger = LoggerFactory.getLogger(QueryExecutor.class.getSimpleName());
 
     private final AnalyticsLoader analyticsLoader;
     private final ExecutorService executorService;
@@ -45,12 +45,21 @@ public class QueryExecutor {
     }
 
     public <T extends ActionRequest> ActionResponse execute(T request) throws FoxtrotException {
+        validate(request);
         Action action = resolve(request);
         return action.execute();
     }
 
     public <T extends ActionRequest> AsyncDataToken executeAsync(T request) throws FoxtrotException {
+        request.validate();
         return resolve(request).execute(executorService);
+    }
+
+    public <T extends ActionRequest> void validate(T request) throws FoxtrotException {
+        Set<String> validationErrors = request.validate();
+        if (!CollectionUtils.isSetNullOrEmpty(validationErrors)) {
+            throw FoxtrotExceptions.createMalformedQueryException(request, new ArrayList<>(validationErrors));
+        }
     }
 
     public <T extends ActionRequest> Action resolve(T request) throws FoxtrotException {
