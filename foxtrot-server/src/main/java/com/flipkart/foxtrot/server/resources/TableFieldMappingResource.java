9f372b712feb3c0d6d5b4ce6952e0b894e7bfbba
Santanu Sinha <santanu.sinha@gmail.com>
2016-11-03 10:19:46 +0530

    Added a all tables meta api

--- foxtrot-server/src/main/java/com/flipkart/foxtrot/server/resources/TableFieldMappingResource.java
+++ foxtrot-server/src/main/java/com/flipkart/foxtrot/server/resources/TableFieldMappingResource.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014 Flipkart Internet Pvt. Ltd.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,8 +15,10 @@
  */
 package com.flipkart.foxtrot.server.resources;
 
+import com.flipkart.foxtrot.common.Table;
 import com.flipkart.foxtrot.core.exception.FoxtrotException;
 import com.flipkart.foxtrot.core.querystore.QueryStore;
+import com.flipkart.foxtrot.core.table.TableManager;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -24,23 +26,44 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.stream.Collectors;
 
 /**
  * Created by rishabh.goyal on 06/05/14.
  */
-@Path("/v1/tables/{name}/fields")
+@Path("/v1/tables")
 @Produces(MediaType.APPLICATION_JSON)
 public class TableFieldMappingResource {
 
-    private QueryStore queryStore;
+    private final TableManager tableManager;
+    private final QueryStore queryStore;
 
-    public TableFieldMappingResource(QueryStore queryStore) {
+    public TableFieldMappingResource(TableManager tableManager, QueryStore queryStore) {
+        this.tableManager = tableManager;
         this.queryStore = queryStore;
     }
 
     @GET
+    @Path("/{name}/fields")
     public Response get(@PathParam("name") final String table) throws FoxtrotException {
         return Response.ok(queryStore.getFieldMappings(table)).build();
     }
 
+
+    @GET
+    @Path("/fields")
+    public Response getAllFields() throws FoxtrotException {
+        return Response.ok()
+                .entity(tableManager.getAll()
+                        .stream()
+                        .collect(
+                                Collectors.toMap(Table::getName, table -> {
+                                    try {
+                                        return queryStore.getFieldMappings(table.getName());
+                                    } catch (FoxtrotException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                })))
+                .build();
+    }
 }
--- foxtrot-server/src/test/java/com/flipkart/foxtrot/server/resources/TableFieldMappingResourceTest.java
+++ foxtrot-server/src/test/java/com/flipkart/foxtrot/server/resources/TableFieldMappingResourceTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014 Flipkart Internet Pvt. Ltd.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,25 +19,33 @@ import com.flipkart.foxtrot.common.FieldType;
 import com.flipkart.foxtrot.common.FieldTypeMapping;
 import com.flipkart.foxtrot.common.TableFieldMapping;
 import com.flipkart.foxtrot.core.TestUtils;
+import com.flipkart.foxtrot.core.querystore.QueryStore;
+import com.flipkart.foxtrot.core.table.impl.FoxtrotTableManager;
 import com.flipkart.foxtrot.server.providers.exception.FoxtrotExceptionMapper;
 import io.dropwizard.testing.junit.ResourceTestRule;
-import org.junit.ClassRule;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by rishabh.goyal on 06/05/14.
  */
 public class TableFieldMappingResourceTest extends FoxtrotResourceTest {
+
+    private final FoxtrotTableManager tableManager;
 
     @Rule
     public ResourceTestRule resources;
@@ -46,8 +54,12 @@ public class TableFieldMappingResourceTest extends FoxtrotResourceTest {
         super();
         doReturn(true).when(getTableMetadataManager()).exists(TestUtils.TEST_TABLE_NAME);
         doReturn(TestUtils.TEST_TABLE).when(getTableMetadataManager()).get(anyString());
+        QueryStore queryStore = getQueryStore();
+
+        tableManager = mock(FoxtrotTableManager.class);
+        when(tableManager.getAll()).thenReturn(Collections.singletonList(TestUtils.TEST_TABLE));
         resources = ResourceTestRule.builder()
-                .addResource(new TableFieldMappingResource(getQueryStore()))
+                .addResource(new TableFieldMappingResource(tableManager, queryStore))
                 .setMapper(getMapper())
                 .addProvider(new FoxtrotExceptionMapper(getMapper()))
                 .build();
@@ -79,7 +91,7 @@ public class TableFieldMappingResourceTest extends FoxtrotResourceTest {
         try {
             resources.client().target(
                     String.format("/v1/tables/%s/fields", TestUtils.TEST_TABLE_NAME + "-missing")).request().head();
-        } catch(WebApplicationException ex) {
+        } catch (WebApplicationException ex) {
             assertEquals(Response.Status.NOT_FOUND.getStatusCode(), ex.getResponse().getStatus());
         }
     }
@@ -93,4 +105,21 @@ public class TableFieldMappingResourceTest extends FoxtrotResourceTest {
         assertEquals(request.getTable(), response.getTable());
         assertNull(response.getMappings());
     }
+
+    @Test
+    public void getAllFields() throws Exception {
+        doNothing().when(getQueryStore()).initializeTable(any(String.class));
+
+        getQueryStore().save(TestUtils.TEST_TABLE_NAME, TestUtils.getMappingDocuments(getMapper()));
+        Thread.sleep(500);
+
+        Map<String, TableFieldMapping> response = resources.client().target("/v1/tables/fields")
+                .request()
+                .get(new GenericType<Map<String, TableFieldMapping>>() {});
+        //System.out.println(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(response));
+        Assert.assertFalse(response.isEmpty());
+        Assert.assertEquals(1, response.size());
+        Assert.assertTrue(response.containsKey(TestUtils.TEST_TABLE_NAME));
+    }
+
 }
