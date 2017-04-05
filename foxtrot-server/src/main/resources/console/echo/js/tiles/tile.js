b829388c52e1644a397293185c5f415f47b0b45a
amarnath <amarnath@innoventes.co>
2017-04-05 09:42:59 +0530

    displayed no data available in health text

--- foxtrot-server/src/main/resources/console/echo/js/tiles/tile.js
+++ foxtrot-server/src/main/resources/console/echo/js/tiles/tile.js
@@ -290,7 +290,7 @@ TileFactory.prototype.create = function () {
     tileElement.find(".trend-chart").remove();
     tileElement.find(".chart-item").addClass("radar-chart");
   } else if(this.tileObject.chartType == "line" || this.tileObject.chartType == "stacked" || this.tileObject.chartType == "stackedBar" || this.tileObject.chartType == "pie") {
-    /*tileElement.find(".widget-header").append('<div id="'+this.tileObject.id+'-health-text" class="lineGraph-health-text">10,000</div>');*/
+    tileElement.find(".widget-header").append('<div id="'+this.tileObject.id+'-health-text" class="lineGraph-health-text">No Data available</div>');
 	 tileElement.find(".widget-header").append('<div id="'+this.tileObject.id+'-health" style=""></div>');
 	 tileElement.find(".chart-item").append('<div id="'+this.tileObject.id+'"></div>');
   }
