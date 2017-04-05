07eda5f36cff2626396519c5c8fc9a7859fe5660
amarnath <amarnath@innoventes.co>
2017-04-05 10:35:51 +0530

    initial commit for changing tabs button

--- foxtrot-server/src/main/resources/console/echo/js/main.js
+++ foxtrot-server/src/main/resources/console/echo/js/main.js
@@ -392,6 +392,20 @@ function loadConsole() {
 	})*/
 }
 
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  //document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
 $(document).ready(function(){
 	var type = $("#widgetType").val();
 	var foxtrot = new FoxTrot();
