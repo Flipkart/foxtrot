1fccb12b901d5d54550d21da11f6a4b9312141ce
amarnath <amarnath@innoventes.co>
2017-05-08 15:24:17 +0530

    removed unused code

--- foxtrot-server/src/main/resources/console/echo/js/tiles/stacked-bar-tile.js
+++ foxtrot-server/src/main/resources/console/echo/js/tiles/stacked-bar-tile.js
@@ -270,42 +270,6 @@ StackedBarTile.prototype.render = function (d) {
     }).appendTo("body").fadeIn(200).fadeOut(50000);
   }
 
-//  var updateLegendTimeout = null;
-//  var latestPosition = null;
-//
-//  function updateLegend() {
-//    updateLegendTimeout = null;
-//
-//    var pos = latestPosition;
-//
-//    var axes = plot.getAxes();
-//    if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max || pos.y < axes.yaxis.min || pos.y > axes.yaxis.max) return;
-//
-//    var i, j, dataset = plot.getData();
-//    for (i = 0; i < dataset.length; ++i) {
-//      var series = dataset[i];
-//
-//      // find the nearest points, x-wise
-//      for (j = 0; j < series.data.length; ++j)
-//        if (series.data[j][0] > pos.x) break;
-//
-//      // now interpolate
-//      var y, p1 = series.data[j - 1],
-//          p2 = series.data[j];
-//      if (p1 == null) y = p2[1];
-//      else if (p2 == null) y = p1[1];
-//      else y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
-//      console.log(y)
-//      console.log(series.label)
-//      showTooltip(pos.x, pos.y, y, "");
-//      chartDiv.find(".legend").eq(i).text(series.label.replace(/=.*/, "= " + y.toFixed(2) + " °C"));
-//    }
-//  }
-//
-//  $(ctx).bind("plothover", function (event, pos, item) {
-//    latestPosition = pos;
-//    if (!updateLegendTimeout) updateLegendTimeout = setTimeout(updateLegend, 50);
-//  });
   var previousPoint = null;
   $(ctx).bind("plothover", function (event, pos, item) {
     if (item) {
@@ -324,7 +288,6 @@ StackedBarTile.prototype.render = function (d) {
             strTip += "</br>"+ p[1] + " for " + "<span style="+s.color+">"+s.label+"<span>";
           }
           else {
-            console.log('===>')
             $("#tooltip").remove();
             previousPoint = null;
           }
