0ec10bd6a2c0a7156dc002d6245a6b506472e74a
amarnath <GautamChibde@bitbucket.org>
2017-05-19 14:58:24 +0530

    fixed deleted ui filter button

--- foxtrot-server/src/main/resources/console/echo/js/tiles/tile.js
+++ foxtrot-server/src/main/resources/console/echo/js/tiles/tile.js
@@ -52,9 +52,9 @@ function refereshTiles() {
   }
 }
 
-//setInterval(function () {
-//  refereshTiles();
-//}, 6000);
+setInterval(function () {
+  refereshTiles();
+}, 6000);
 
 function pushTilesObject(object) {
   tileData[object.id] = object;
