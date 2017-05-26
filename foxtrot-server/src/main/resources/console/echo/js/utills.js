1ee5a736fb108701c9f463571867f363aeeda33f
amarnath <GautamChibde@bitbucket.org>
2017-05-26 15:02:22 +0530

    fixed number display issue

--- foxtrot-server/src/main/resources/console/echo/js/utills.js
+++ foxtrot-server/src/main/resources/console/echo/js/utills.js
@@ -349,7 +349,18 @@ function unique(list) {
 }
 
 function numberWithCommas(x) {
-  return x.toLocaleString();
+  x=x.toString();
+  var afterPoint = '';
+  if(x.indexOf('.') > 0)
+    afterPoint = x.substring(x.indexOf('.'),x.length);
+  x = Math.floor(x);
+  x=x.toString();
+  var lastThree = x.substring(x.length-3);
+  var otherNumbers = x.substring(0,x.length-3);
+  if(otherNumbers != '')
+    lastThree = ',' + lastThree;
+  var res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree + afterPoint;
+  return res;
 }
 
 function deleteWidget(id) {
