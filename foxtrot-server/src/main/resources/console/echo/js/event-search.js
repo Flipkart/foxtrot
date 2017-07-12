f3c774bd77ead9bd9e749df043f330dc5c75a61c
amarnath <GautamChibde@bitbucket.org>
2017-07-12 17:17:28 +0530

    initial commit for saving selected and unselected list of checkboxes

--- foxtrot-server/src/main/resources/console/echo/js/event-search.js
+++ foxtrot-server/src/main/resources/console/echo/js/event-search.js
@@ -4,26 +4,30 @@ var browseFilterRowArray = [];
 var currentFieldList = [];
 var tableFiledsArray = {};
 var currentTable = "";
+var headerList = [];
+var rowList = [];
+var selectedList = [];
 
 function getBrowseTables() {
   var select = $(".browse-table");
   $.ajax({
-    url: apiUrl+"/v1/tables/",
-    contentType: "application/json",
-    context: this,
-    success: function(tables) {
+    url: apiUrl + "/v1/tables/"
+    , contentType: "application/json"
+    , context: this
+    , success: function (tables) {
       for (var i = tables.length - 1; i >= 0; i--) {
         tableNameList.push(tables[i].name);
 
       }
-      $.each(tableNameList, function(key, value) {
+      $.each(tableNameList, function (key, value) {
         $(select).append($("<option></option>")
-                         .attr("value",value)
-                         .text(value));
+          .attr("value", value)
+          .text(value));
       });
       select.selectpicker('refresh');
       select.change();
-    }});
+    }
+  });
 }
 
 getBrowseTables();
@@ -49,11 +53,11 @@ function queryTypeTriggered(el) {
   var rowString = $(el).attr('id');
   var rowIdArray = rowString.split('-');
   var rowId = rowIdArray[2];
-  $('#filter-column-row-'+rowId).val('');
-  if(columnType == "STRING") {
-    $('#filter-column-row-'+rowId).prop("type", "text");
-  } else if(columnType == "LONG") {
-    $('#filter-column-row-'+rowId).prop("type", "number");
+  $('#filter-column-row-' + rowId).val('');
+  if (columnType == "STRING") {
+    $('#filter-column-row-' + rowId).prop("type", "text");
+  } else if (columnType == "LONG") {
+    $('#filter-column-row-' + rowId).prop("type", "number");
   }
 }
 
@@ -61,15 +65,15 @@ function setBetweenInput(el) {
   var selectedType = $(el).val();
   var rowString = $(el).attr('id');
   var rowId = parseInt(rowString);
-  if(selectedType == "between") {
-    $('#filter-between-input-'+rowId).prop("disabled", false);
+  if (selectedType == "between") {
+    $('#filter-between-input-' + rowId).prop("disabled", false);
   } else {
-    $('#filter-between-input-'+rowId).prop("disabled", true);
+    $('#filter-between-input-' + rowId).prop("disabled", true);
   }
-  $('#filter-between-input-'+rowId).val("");
+  $('#filter-between-input-' + rowId).val("");
 }
 
-$( ".browse-table" ).change(function() {
+$(".browse-table").change(function () {
   var tableId = this.value;
   fetchFields(tableId);
   currentTable = tableId;
@@ -96,42 +100,69 @@ function runQuery() {
   var filterSection = $("#browse-events-form");
   var fromDate = $('input[name="daterange"]').daterangepicker.from;
   var toDate = $('input[name="daterange"]').daterangepicker.toDate;
-  if((fromDate - toDate) > 1000) {
+  if ((fromDate - toDate) > 1000) {
     filters.push({
-      field: "_timestamp",
-      operator: "between",
-      from: fromDate,
-      to: toDate
+      field: "_timestamp"
+      , operator: "between"
+      , from: fromDate
+      , to: toDate
     });
   }
 
   var table = currentTable;
   var request = {
-    opcode: "query",
-    table: table,
-    filters: filters,
-    sort: {
-      field: "_timestamp",
-      order: filterSection.find("#dataSort").val()
-    },
-    from: 0,
-    limit: 10
+    opcode: "query"
+    , table: table
+    , filters: filters
+    , sort: {
+      field: "_timestamp"
+      , order: filterSection.find("#dataSort").val()
+    }
+    , from: 0
+    , limit: 10
   };
   $.ajax({
-    method: 'POST',
-    url: apiUrl+"/v1/analytics",
-    contentType: "application/json",
-    data: JSON.stringify(request),
-    dataType: 'json',
-    success: function(resp) {
+    method: 'POST'
+    , url: apiUrl + "/v1/analytics"
+    , contentType: "application/json"
+    , data: JSON.stringify(request)
+    , dataType: 'json'
+    , success: function (resp) {
       renderTable(resp);
-  }
+    }
   });
   console.log(request);
 }
 
+function generateColumChooserList() {
+  var parent = $("#column-chooser");
+  var listElement = parent.find("#column-list");
+  for (var column in headerList) {
+    if (column <= 9) {
+      listElement.append("<label><input type='checkbox' value='" + headerList[column] + "' class='column-chooser' checked> &nbsp;&nbsp;&nbsp;" + headerList[column] + "</label><br/>");
+      selectedList.push(headerList[column]);
+    } else {
+      listElement.append("<label><input type='checkbox' value='" + headerList[column] + "' class='column-chooser'> &nbsp;&nbsp;&nbsp;" + headerList[column] + "</label><br/>");
+    }
+  }
+
+  var selections = []
+    , render_selections = function () {
+      selections.join(', ');
+      selectedList = [];
+      selectedList = selections;
+    };
+
+  $('.column-chooser').change(function () {
+    selections = $.map($('input[type="checkbox"]:checked'), function (a) {
+      return a.value;
+    })
+    render_selections();
+  });
+}
+
 function renderTable(data) {
-  if(!data.hasOwnProperty("documents") || data.documents.length == 0) {
+  if (!data.hasOwnProperty("documents") || data.documents.length == 0) {
     return;
   }
   var parent = $(".event-display-container");
@@ -142,12 +173,12 @@ function renderTable(data) {
 
   for (var i = data.documents.length - 1; i >= 0; i--) {
     var flatObject = flat.flatten(data.documents[i]);
-    for(field in flatObject) {
-      if(flatObject.hasOwnProperty(field)) {
-        if(field === "id" || field === "timestamp") {
+    for (field in flatObject) {
+      if (flatObject.hasOwnProperty(field)) {
+        if (field === "id" || field === "timestamp") {
           continue;
         }
-        headerMap[field]=1;
+        headerMap[field] = 1;
       }
     }
     flatRows.push(flatObject);
@@ -158,45 +189,50 @@ function renderTable(data) {
     var flatData = flatRows[i];
     for (var j = 0; j < headers.length; j++) {
       var header = headers[j];
-      if(flatData.hasOwnProperty(header)) {
+      if (flatData.hasOwnProperty(header)) {
         row.push(flatData[header]);
-      }
-      else {
+      } else {
         row.push("");
       }
     }
     rows.push(row);
   }
   for (var j = 0; j < headers.length; j++) {
-    headers[j] = headers[j].replace("data.","");
+    headers[j] = headers[j].replace("data.", "");
   }
-  var tableData = {headers : headers, data: rows};
+  headerList = headers;
+  rowList = rows;
+  generateColumChooserList();
+  var tableData = {
+    headers: headers
+    , data: rows
+  };
   parent.html(handlebars("#eventbrowser-template", tableData));
 }
 
 
-$( "#browse-events-run-query" ).click(function() {
+$("#browse-events-run-query").click(function () {
   runQuery();
 });
 
-$( "#browse-events-add-query" ).click(function() {
+$("#browse-events-add-query").click(function () {
   currentFieldList = tableFiledsArray[currentTable].mappings;
   var filterCount = browseFilterRowArray.length;
 
-  if(browseFilterRowArray.length == 0) {
+  if (browseFilterRowArray.length == 0) {
     browseFilterRowArray.push(filterCount);
   } else {
     filterCount = browseFilterRowArray[browseFilterRowArray.length - 1] + 1;
     browseFilterRowArray.push(filterCount);
   }
 
-  var filterRow = '<div class="row clearfix" id="filter-row-' + filterCount + '"><img src="img/remove.png" class="browse-events-filter-remove-img browse-events-delete" id="'+filterCount+'" /><div class="col-sm-3"><select class="selectpicker form-control filter-column filter-background" id="filter-row-' + filterCount + '" data-live-search="true"><option>select</option></select></div><div class="col-sm-3"><select class="selectpicker filter-type filter-background form-control" id="'+filterCount+'" data-live-search="true"><option>select</option><option value="equals">Equal to</option><option value="not_equals">Not Equal to</option><option value="less_than">Less than</option><option value="less_equal">Less or equal to</option><option value="greater_than">Greater than</option><option value="greater_equal">Greater or equal to</option><option value="contains">Equals</option><option value="not_equals">Not equals</option><option value="contains">Contains</option><option value="between">Between</option></select></div><div class="col-sm-3"><input id="filter-column-row-' + filterCount + '" type="text" class="form-control browse-events-filter-value form-control"></div><div class="col-sm-3"><input id="filter-between-input-' + filterCount + '" type="text" class="form-control browse-events-filter-between-value form-control" disabled></div></span></div></div>';
+  var filterRow = '<div class="row clearfix" id="filter-row-' + filterCount + '"><img src="img/remove.png" class="browse-events-filter-remove-img browse-events-delete" id="' + filterCount + '" /><div class="col-sm-3"><select class="selectpicker form-control filter-column filter-background" id="filter-row-' + filterCount + '" data-live-search="true"><option>select</option></select></div><div class="col-sm-3"><select class="selectpicker filter-type filter-background form-control" id="' + filterCount + '" data-live-search="true"><option>select</option><option value="equals">Equal to</option><option value="not_equals">Not Equal to</option><option value="less_than">Less than</option><option value="less_equal">Less or equal to</option><option value="greater_than">Greater than</option><option value="greater_equal">Greater or equal to</option><option value="contains">Equals</option><option value="not_equals">Not equals</option><option value="contains">Contains</option><option value="between">Between</option></select></div><div class="col-sm-3"><input id="filter-column-row-' + filterCount + '" type="text" class="form-control browse-events-filter-value form-control"></div><div class="col-sm-3"><input id="filter-between-input-' + filterCount + '" type="text" class="form-control browse-events-filter-between-value form-control" disabled></div></span></div></div>';
   $(".browse-rows").append(filterRow);
   var filterValueEl = $("#filter-row-" + filterCount).find('.browse-events-delete');
   var filterType = $("#filter-row-" + filterCount).find('.filter-type');
   $(filterType).selectpicker('refresh');
   var filterColumn = $("#filter-row-" + filterCount).find('.filter-column')
-  setTimeout(function(){
+  setTimeout(function () {
     generateDropDown(currentFieldList, filterColumn);
   }, 0);
 
@@ -212,13 +248,13 @@ $( "#browse-events-add-query" ).click(function() {
 });
 
 function showHideColumnChooser() { // page setting modal
-  console.log('==')
-  if( $('#column-chooser').is(':visible') ) {
+  if ($('#column-chooser').is(':visible')) {
     $('#column-chooser').hide();
-  }
-  else {
+  } else {
     $('#column-chooser').show();
-    $('#column-chooser').css({ 'width': '356px' });
+    $('#column-chooser').css({
+      'width': '356px'
+    });
   }
 }
 
