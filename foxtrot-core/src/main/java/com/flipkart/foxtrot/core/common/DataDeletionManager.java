e4aaf83d96b64b649093514d69fa2206e17afb71
nitish.goyal <nitish.goyal@phonepe.com>
2018-09-13 08:17:42 +0530

    Data deletion minor fix

--- foxtrot-core/src/main/java/com/flipkart/foxtrot/core/common/DataDeletionManager.java
+++ foxtrot-core/src/main/java/com/flipkart/foxtrot/core/common/DataDeletionManager.java
@@ -47,8 +47,7 @@ public class DataDeletionManager implements Managed {
                     LockingTaskExecutor executor =
                             new DefaultLockingTaskExecutor(new HazelcastLockProvider(hazelcastConnection.getHazelcast()));
                     Instant lockAtMostUntil = Instant.now().plusSeconds(TimeUnit.HOURS.toSeconds(MAX_TIME_TO_RUN_TASK_IN_HOURS));
-                    executor.executeWithLock(new DataDeletionTask(queryStore),
-                                             new LockConfiguration("cardinalityCalculation", lockAtMostUntil));
+                    executor.executeWithLock(new DataDeletionTask(queryStore), new LockConfiguration("dataDeletion", lockAtMostUntil));
 
                 }
             }, config.getInitialDelay(), config.getInterval(), TimeUnit.SECONDS);
