8de9d3ef9cdd62023a2a6aaf198ddb13760361cb
nitish.goyal <nitish.goyal@phonepe.com>
2018-09-20 09:59:05 +0530

    Making caching configurable

--- foxtrot-server/src/main/java/com/flipkart/foxtrot/server/config/FoxtrotServerConfiguration.java
+++ foxtrot-server/src/main/java/com/flipkart/foxtrot/server/config/FoxtrotServerConfiguration.java
@@ -20,6 +20,7 @@ import com.flipkart.foxtrot.core.cardinality.CardinalityConfig;
 import com.flipkart.foxtrot.core.common.DataDeletionManagerConfig;
 import com.flipkart.foxtrot.core.datastore.impl.hbase.HbaseConfig;
 import com.flipkart.foxtrot.core.jobs.optimization.EsIndexOptimizationConfig;
+import com.flipkart.foxtrot.core.querystore.impl.CacheConfig;
 import com.flipkart.foxtrot.core.querystore.impl.ClusterConfig;
 import com.flipkart.foxtrot.core.querystore.impl.ElasticsearchConfig;
 import io.dropwizard.Configuration;
@@ -63,6 +64,8 @@ public class FoxtrotServerConfiguration extends Configuration {
     @Valid
     @JsonProperty("deletionconfig")
     private final DataDeletionManagerConfig deletionManagerConfig;
+
+    private CacheConfig cacheConfig;
 
     public FoxtrotServerConfiguration() {
         this.hbase = new HbaseConfig();
