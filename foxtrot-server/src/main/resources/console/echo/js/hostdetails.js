4308ec96eed23dd9505820cb68655b26a880db7f
amarnath innoventes <amarnath.innoventes@phonepe.com>
2018-09-24 10:58:49 +0530

    correct host url

--- foxtrot-server/src/main/resources/console/echo/js/hostdetails.js
+++ foxtrot-server/src/main/resources/console/echo/js/hostdetails.js
@@ -1,5 +1,5 @@
 function getHostUrl() {
-  return "http://localhost:17000/foxtrot";
+  //return "http://localhost:17000/foxtrot";
   //return "https://foxtrot-internal.phonepe.com/foxtrot";
-  //return location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '')+"/foxtrot";
+  return location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '')+"/foxtrot";
 }
