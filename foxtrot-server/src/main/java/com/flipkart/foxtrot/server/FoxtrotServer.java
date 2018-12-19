ece6cb92cdef3f5b9771f7b10f12d3eac325a417
Nitish Goyal <nitish.goyal@phonepe.com>
2018-12-19 17:01:00 +0530

    Minor change

--- foxtrot-server/src/main/java/com/flipkart/foxtrot/server/FoxtrotServer.java
+++ foxtrot-server/src/main/java/com/flipkart/foxtrot/server/FoxtrotServer.java
@@ -142,7 +142,7 @@ public class FoxtrotServer extends Application<FoxtrotServerConfiguration> {
                 .threads(1)
                 .build();
 
-        HbaseTableConnection HBaseTableConnection = new HbaseTableConnection(configuration.getHbase());
+        HbaseTableConnection hbaseTableConnection = new HbaseTableConnection(configuration.getHbase());
         ElasticsearchConnection elasticsearchConnection = new ElasticsearchConnection(configuration.getElasticsearch());
         HazelcastConnection hazelcastConnection = new HazelcastConnection(configuration.getCluster());
         ElasticsearchUtils.setTableNamePrefix(configuration.getElasticsearch());
@@ -164,7 +164,7 @@ public class FoxtrotServer extends Application<FoxtrotServerConfiguration> {
                                                                                         elasticsearchConnection,
                                                                                         objectMapper, cardinalityConfig
         );
-        DataStore dataStore = new HBaseDataStore(HBaseTableConnection, objectMapper,
+        DataStore dataStore = new HBaseDataStore(hbaseTableConnection, objectMapper,
                                                  new DocumentTranslator(configuration.getHbase())
         );
         QueryStore queryStore = new ElasticsearchQueryStore(tableMetadataManager, elasticsearchConnection, dataStore,
@@ -199,7 +199,7 @@ public class FoxtrotServer extends Application<FoxtrotServerConfiguration> {
         );
 
         environment.lifecycle()
-                .manage(HBaseTableConnection);
+                .manage(hbaseTableConnection);
         environment.lifecycle()
                 .manage(elasticsearchConnection);
         environment.lifecycle()
