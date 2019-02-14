c6ebef67187dc5a8199035b270ac0e213fa7ff76
amarnath innoventes <amarnath.innoventes@phonepe.com>
2019-02-14 12:12:26 +0530

    moved login check code to main.js and fixed undefined issues

--- foxtrot-server/src/main/resources/console/echo/js/main.js
+++ foxtrot-server/src/main/resources/console/echo/js/main.js
@@ -1110,4 +1110,8 @@ $(document).ready(function () {
     refereshTiles();
     $("#myModal").modal("hide");    
   })
+
+  setTimeout(function() { isLoggedIn(); // check user is logged in
+  }, 3000);
+
 });
