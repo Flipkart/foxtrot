7b679715adfd96b474012441c85056e07633f864
amarnath innoventes <amarnath.innoventes@phonepe.com>
2019-02-14 11:36:26 +0530

    fixed undefined issue in slow connection

--- foxtrot-server/src/main/resources/console/echo/js/utills.js
+++ foxtrot-server/src/main/resources/console/echo/js/utills.js
@@ -566,7 +566,7 @@ function getCookieConstant() {
  * Get login redirect url
  */
 function getLoginRedirectUrl() {
-  return "http://gandalf.traefik.stg.phonepe.com/echo/index.html";
+  return "http://gandalf.traefik.stg.phonepe.com/echo";
 }
 
 /**
