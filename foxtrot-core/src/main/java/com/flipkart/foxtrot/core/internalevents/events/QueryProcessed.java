aefd18e93241f71759451331f1e214216d888099
Santanu Sinha <santanu.sinha@gmail.com>
2019-07-16 17:30:32 +0530

    Internal event system

---
+++ foxtrot-core/src/main/java/com/flipkart/foxtrot/core/internalevents/events/QueryProcessed.java
@@ -0,0 +1,24 @@
+package com.flipkart.foxtrot.core.internalevents.events;
+
+import com.flipkart.foxtrot.common.ActionRequest;
+import com.flipkart.foxtrot.common.ActionResponse;
+import com.flipkart.foxtrot.core.internalevents.InternalSystemEvent;
+import com.flipkart.foxtrot.core.internalevents.InternalSystemEventVisitor;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ *
+ */
+@Data
+@Builder
+public class QueryProcessed implements InternalSystemEvent {
+    private final ActionRequest request;
+    private final ActionResponse response;
+    private final long evaluationDurationMs;
+
+    @Override
+    public <T> T accept(InternalSystemEventVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+}
---
+++ foxtrot-core/src/main/java/com/flipkart/foxtrot/core/internalevents/events/QueryProcessingError.java
@@ -0,0 +1,23 @@
+package com.flipkart.foxtrot.core.internalevents.events;
+
+import com.flipkart.foxtrot.common.ActionRequest;
+import com.flipkart.foxtrot.core.exception.FoxtrotException;
+import com.flipkart.foxtrot.core.internalevents.InternalSystemEvent;
+import com.flipkart.foxtrot.core.internalevents.InternalSystemEventVisitor;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ *
+ */
+@Data
+@Builder
+public class QueryProcessingError implements InternalSystemEvent  {
+    private final ActionRequest request;
+    private final FoxtrotException exception;
+
+    @Override
+    public <T> T accept(InternalSystemEventVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+}
