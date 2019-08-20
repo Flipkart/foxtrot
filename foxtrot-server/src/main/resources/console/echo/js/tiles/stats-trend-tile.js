e589c013ec8f69ff46af33245febf18143f4b0b4
amarnath innoventes <amarnath.innoventes@phonepe.com>
2019-08-20 15:09:08 +0530

    template filters

--- foxtrot-server/src/main/resources/console/echo/js/tiles/stats-trend-tile.js
+++ foxtrot-server/src/main/resources/console/echo/js/tiles/stats-trend-tile.js
@@ -79,6 +79,12 @@ StatsTrendTile.prototype.getQuery = function(object) {
       filters.push(object.tileContext.filters[i]);
     }
   }
+
+  var templateFilters = isAppendTemplateFilters(object.tileContext.table);
+  if(templateFilters.length > 0) {
+    filters = filters.concat(templateFilters);
+  }
+  
   var data = {
     "opcode": "statstrend",
     "table": object.tileContext.table,
