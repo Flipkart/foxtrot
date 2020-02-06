977305d372030cf3e3e0882c1b43ea62d3f0a2a7
Jitendra Dhawan <jitendra.dhawan@phonepe.com>
2020-02-06 13:43:00 +0530

    add blank space

--- foxtrot-core/src/test/java/com/flipkart/foxtrot/core/table/impl/ElasticsearchTestUtils.java
+++ foxtrot-core/src/test/java/com/flipkart/foxtrot/core/table/impl/ElasticsearchTestUtils.java
@@ -4,6 +4,7 @@ package com.flipkart.foxtrot.core.table.impl;
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
+ *
  * You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
