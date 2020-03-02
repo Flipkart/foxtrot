b809e0e1553a24ed886d9b396022d7d5e8fffabc
Nitish Goyal <nitish.goyal@phonepe.com>
2020-03-02 14:59:32 +0530

    Uncomment HBase save

--- foxtrot-core/src/main/java/com/flipkart/foxtrot/core/datastore/impl/hbase/HBaseDataStore.java
+++ foxtrot-core/src/main/java/com/flipkart/foxtrot/core/datastore/impl/hbase/HBaseDataStore.java
@@ -135,13 +135,13 @@ public class HBaseDataStore implements DataStore {
             throw FoxtrotExceptions.createBadRequestException(table.getName(), errorMessages);
         }
 
-        /*try (org.apache.hadoop.hbase.client.Table hTable = tableWrapper.getTable(table)) {
+        try (org.apache.hadoop.hbase.client.Table hTable = tableWrapper.getTable(table)) {
             hTable.put(puts);
         }
         catch (IOException e) {
             logger.error("Error occurred while ingesting event in HBase : ", e);
             throw FoxtrotExceptions.createConnectionException(table, e);
-        }*/
+        }
         return translatedDocuments.build();
     }
 
