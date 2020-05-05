849be62b38564e5fa763c41fe1c4d8a8aadfd369
ShashiKiran TP <shashikiran.invts@Shashikiran-Phonepes-MacBook-Pro.local>
2020-05-05 14:14:42 +0530

    adding download image to widget

--- foxtrot-server/src/main/resources/console/echo/js/hostdetails.js
+++ foxtrot-server/src/main/resources/console/echo/js/hostdetails.js
@@ -1,4 +1,6 @@
 function getHostUrl() {
   //return "http://localhost:17000/foxtrot";
-  return location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '')+"/foxtrot";
+  return "http://foxtrot.traefik.stg.phonepe.com/foxtrot"
+  return location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + "/foxtrot";
+
 }
