bf330966dfd6667498eff68ab40409dd5089f34d
ShashiKiran TP <shashikiran.invts@Shashikiran-Phonepes-MacBook-Pro.local>
2020-06-01 23:23:55 +0530

    global-filter-period-select  margin UI changed

--- foxtrot-server/src/main/resources/console/echo/js/hostdetails.js
+++ foxtrot-server/src/main/resources/console/echo/js/hostdetails.js
@@ -1,5 +1,5 @@
 function getHostUrl() {
   //return "http://localhost:17000/foxtrot";
+  return "http://foxtrot.traefik.stg.phonepe.com/foxtrot"
   return location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + "/foxtrot";
-
 }
