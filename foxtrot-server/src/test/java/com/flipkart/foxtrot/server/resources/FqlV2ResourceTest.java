1978f8e2d2384d58e8a842c5e74b1d3f8964a44f
Naman Jain <naman.jain@PP-C02YQC3WLVCG.local>
2020-06-01 20:59:36 +0530

    added few more testcases with code refactorings

--- foxtrot-server/src/test/java/com/flipkart/foxtrot/server/resources/FqlV2ResourceTest.java
+++ foxtrot-server/src/test/java/com/flipkart/foxtrot/server/resources/FqlV2ResourceTest.java
@@ -2,6 +2,9 @@ package com.flipkart.foxtrot.server.resources;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.flipkart.foxtrot.common.TableActionRequestVisitor;
+import com.flipkart.foxtrot.common.exception.ErrorCode;
+import com.flipkart.foxtrot.common.exception.FoxtrotException;
+import com.flipkart.foxtrot.common.exception.FqlParsingException;
 import com.flipkart.foxtrot.core.config.FoxtrotServerConfiguration;
 import com.flipkart.foxtrot.core.config.QueryConfig;
 import com.flipkart.foxtrot.core.exception.provider.FoxtrotExceptionMapper;
@@ -10,9 +13,12 @@ import com.flipkart.foxtrot.sql.FqlEngine;
 import com.flipkart.foxtrot.sql.fqlstore.FqlGetRequest;
 import com.flipkart.foxtrot.sql.fqlstore.FqlStore;
 import com.flipkart.foxtrot.sql.fqlstore.FqlStoreServiceImpl;
+import com.flipkart.foxtrot.sql.responseprocessors.model.FlatRepresentation;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import java.util.List;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -31,29 +37,69 @@ public class FqlV2ResourceTest extends FoxtrotResourceTest {
             .build();
 
     @Test
-    public void test0() {
+    public void testExecuteQuery() {
         String query = "show tables";
         Entity<String> stringEntity = Entity.json(query);
-        resources.client()
+        FlatRepresentation flatRepresentation = resources.client()
+                .target("/v2/fql")
+                .request()
+                .post(stringEntity, FlatRepresentation.class);
+
+        Assert.assertNotNull(flatRepresentation);
+        Assert.assertEquals(2, flatRepresentation.getHeaders().size());
+        Assert.assertEquals("name", flatRepresentation.getHeaders().get(0).getName());
+        Assert.assertEquals("ttl", flatRepresentation.getHeaders().get(1).getName());
+        Assert.assertEquals(1, flatRepresentation.getRows().size());
+        Assert.assertEquals("test-table", flatRepresentation.getRows().get(0).get("name"));
+        Assert.assertEquals(7, flatRepresentation.getRows().get(0).get("ttl"));
+        Assert.assertEquals(4, flatRepresentation.getRows().get(0).get("defaultRegions"));
+    }
+
+    @Test
+    public void testExecuteQueryParseFail() {
+        String query = "select * from test1 where";
+        Entity<String> stringEntity = Entity.json(query);
+        Response response = resources.client()
                 .target("/v2/fql")
                 .request()
                 .post(stringEntity);
+        Assert.assertEquals(Status.INTERNAL_SERVER_ERROR.getStatusCode(), response.getStatus());
+        FoxtrotException fqlParseError = response.readEntity(FqlParsingException.class);
+        Assert.assertEquals(ErrorCode.FQL_PARSE_ERROR, fqlParseError.getCode());
     }
 
     @Test
-    public void test() {
+    public void testNullResponseFromExecuteQuery() {
+        String query = "select * from test1";
+        Entity<String> stringEntity = Entity.json(query);
+        Response response = resources.client()
+                .target("/v2/fql")
+                .request()
+                .post(stringEntity);
+        Assert.assertEquals(Status.NO_CONTENT.getStatusCode(), response.getStatus());
+    }
+
+    @Test
+    public void testSaveFqlStore() {
+        String title = "testQuery";
+        String query = "select * from test";
+
         FqlStore fqlStore = new FqlStore();
-        fqlStore.setTitle("testQuery");
-        fqlStore.setQuery("select * from test");
+        fqlStore.setTitle(title);
+        fqlStore.setQuery(query);
         Entity<FqlStore> fqlStoreEntity = Entity.json(fqlStore);
-        resources.client()
+        FqlStore fqlStoreResponse = resources.client()
                 .target("/v2/fql/save")
                 .request()
-                .post(fqlStoreEntity);
+                .post(fqlStoreEntity, FqlStore.class);
+
+        Assert.assertNotNull(fqlStoreResponse);
+        Assert.assertEquals(title, fqlStoreResponse.getTitle());
+        Assert.assertEquals(query, fqlStoreResponse.getQuery());
     }
 
     @Test
-    public void test1() throws InterruptedException {
+    public void testGetSavedFqlStore() throws InterruptedException {
         String title = "title1";
         String query = "show tables";
 
@@ -61,24 +107,27 @@ public class FqlV2ResourceTest extends FoxtrotResourceTest {
         fqlStore.setTitle(title);
         fqlStore.setQuery(query);
         Entity<FqlStore> fqlStoreEntity = Entity.json(fqlStore);
-        FqlStore fqlStore1 = resources.client()
+
+        FqlStore fqlStoreResponse = resources.client()
                 .target("/v2/fql/save")
                 .request()
                 .post(fqlStoreEntity, FqlStore.class);
-        Assert.assertNotNull(fqlStore1);
-        Assert.assertEquals(title, fqlStore1.getTitle());
-        Assert.assertEquals(query, fqlStore1.getQuery());
+        Assert.assertNotNull(fqlStoreResponse);
+        Assert.assertEquals(title, fqlStoreResponse.getTitle());
+        Assert.assertEquals(query, fqlStoreResponse.getQuery());
 
         Thread.sleep(1000);
 
         FqlGetRequest fqlGetRequest = new FqlGetRequest();
         fqlGetRequest.setTitle(title);
         Entity<FqlGetRequest> fqlGetRequestEntity = Entity.json(fqlGetRequest);
-        List<FqlStore> result = getMapper().convertValue(resources.client()
-                .target("/v2/fql/get")
-                .request()
-                .post(fqlGetRequestEntity, List.class), new TypeReference<List<FqlStore>>() {
-        });
+        List<FqlStore> result = getMapper().convertValue(
+                resources.client()
+                        .target("/v2/fql/get")
+                        .request()
+                        .post(fqlGetRequestEntity, List.class),
+                new TypeReference<List<FqlStore>>(){});
+
         Assert.assertEquals(1, result.size());
         Assert.assertEquals(title, result.get(0)
                 .getTitle());
