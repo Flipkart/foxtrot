93293a7a2350971f4f89f9d16fb391f144df7387
ShashiKiran TP <shashikiran.invts@Shashikiran-Phonepes-MacBook-Pro.local>
2020-06-19 14:57:56 +0530

    added copy Tab to console functionality

--- foxtrot-server/src/main/resources/console/echo/js/hostdetails.js
+++ foxtrot-server/src/main/resources/console/echo/js/hostdetails.js
@@ -1,4 +1,5 @@
 function getHostUrl() {
   //return "http://localhost:17000/foxtrot";
+  return "http://foxtrot.traefik.stg.phonepe.com/foxtrot"
   return location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + "/foxtrot";
 }
