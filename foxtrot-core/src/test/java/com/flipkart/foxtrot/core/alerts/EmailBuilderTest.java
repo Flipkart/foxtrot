5d67eaa4859c3ab46d25d4a12dbdc8018ef44cc6
Jitendra Dhawan <jitendra.dhawan@phonepe.com>
2020-06-23 09:10:29 +0530

    fix test caes

--- foxtrot-core/src/test/java/com/flipkart/foxtrot/core/alerts/EmailBuilderTest.java
+++ foxtrot-core/src/test/java/com/flipkart/foxtrot/core/alerts/EmailBuilderTest.java
@@ -35,7 +35,8 @@ public class EmailBuilderTest {
         Assert.assertEquals("Blocked query as it might have screwed up the cluster", email.getSubject());
         Assert.assertEquals(
                 "Blocked Query: {\"opcode\":\"group\",\"filters\":[],\"bypassCache\":false,\"table\":\"test-table\","
-                        + "\"uniqueCountOn\":null,\"nesting\":[\"os\",\"deviceId\"],\"consoleId\":null,\"precision\":null}\n"
+                        +  "\"uniqueCountOn\":null,\"aggregationField\":null,\"stats\":null,\"nesting\":" +
+                        "[\"os\",\"deviceId\"],\"consoleId\":null,\"precision\":null}\n"
                         + "Suspect field: deviceId\n" + "Probability of screwing up the cluster: 0.75",
                 email.getContent());
     }
