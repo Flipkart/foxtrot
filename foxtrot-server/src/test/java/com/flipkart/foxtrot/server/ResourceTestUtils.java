f364c73120c31511cb7c1e1d4baecd1e9937ff98
Jitendra Dhawan <jitendra.dhawan@phonepe.com>
2020-08-27 12:26:58 +0530

    remove roles allowed from resource test utils

--- foxtrot-server/src/test/java/com/flipkart/foxtrot/server/ResourceTestUtils.java
+++ foxtrot-server/src/test/java/com/flipkart/foxtrot/server/ResourceTestUtils.java
@@ -5,7 +5,6 @@ import com.flipkart.foxtrot.core.exception.provider.FoxtrotExceptionMapper;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.glassfish.jersey.test.grizzly.GrizzlyWebTestContainerFactory;
 
 /**
@@ -17,7 +16,6 @@ public class ResourceTestUtils {
     public static ResourceTestRule.Builder testResourceBuilder(ObjectMapper mapper) {
         return ResourceTestRule.builder()
                 .setTestContainerFactory(new GrizzlyWebTestContainerFactory())
-                .addProvider(RolesAllowedDynamicFeature.class)
                 .setMapper(mapper)
                 .addProvider(new FoxtrotExceptionMapper(mapper));
     }
